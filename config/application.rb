4c6141c2bda3887a36ad2c4005f36219058e14f0
Kornel <kornel@kornel.com>
2016-08-01 14:17:53 +0200

    Kolejny commit

--- config/application.rb
+++ config/application.rb
@@ -10,6 +10,9 @@ module Dyplomowa
   class Application < Rails::Application
 
     config.generators do |g|
+      g.assets false
+      g.template_engine false
+      g.jbuilder false
       g.test_framework :rspec,
         fixtures: true,
         view_specs: false,
@@ -26,7 +29,8 @@ module Dyplomowa
 
     # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
-    # config.time_zone = 'Central Time (US & Canada)'
+    config.time_zone = 'Warsaw'
+    config.autoload_paths += Dir["#{config.root}/lib/**/"]
 
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
