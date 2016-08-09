cce815ca99356880c7f615ad6f854630495c7dac
Kornel <kornel@kornel.com>
2016-08-09 09:45:19 +0200

    user_mailer

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,6 +1,6 @@
 class UserMailer < ApplicationMailer
   def service_update(user)
-    @user = User.find(21)
+    @user = User.find(4)
     @url  = 'dyplomowa.dev'
     mail(to: @user.email, subject: 'Welcome to My Awesome Site')
   end
